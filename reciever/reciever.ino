fc56678f37cec8d284c7e698a478ced9cce52ef9
Zachary Liu <liu.zachary@gmail.com>
2012-03-27 18:01:25 -0400

    Added comments.

--- reciever/reciever.ino
+++ reciever/reciever.ino
@@ -1,16 +1,16 @@
 void setup() {
-  Serial.begin(2400);
-  pinMode(13, OUTPUT);
+  Serial.begin(2400); // Initialize serial
+  pinMode(13, OUTPUT); // Initialize the onboard LED
 }
 
 void loop() {
-  if (Serial.available()) {
-    byte recieved = Serial.read();
-    byte data = recieved ^ B10101010;
+  if (Serial.available()) { // Only read data if it is available
+    byte recieved = Serial.read(); // Read the raw data
+    byte data = recieved ^ B10101010; // Process using a bitwise XOR to remove the modulation
     
-    if (data == B00000001) {
+    if (data == B00000001) { // Turn on the LED if it recieved a 1
       digitalWrite(13, HIGH);
-    } else if (data == B00000000) {
+    } else if (data == B00000000) { // Turn off the LED it recieved a 0
       digitalWrite(13, LOW);
     }
   }
--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,18 +1,18 @@
 void setup() {
-  Serial.begin(2400);
-  pinMode(2, INPUT);
+  Serial.begin(2400); // Initialize serial
+  pinMode(2, INPUT); // Initialize the input pin
 }
 
 void loop() {
   byte data;
   
-  if (digitalRead(2) == HIGH) {
+  if (digitalRead(2) == HIGH) { // If the input is 1, set the data to 1, else set the data to 0
     data = B00000001;
   } else {
     data = B00000000;
   }
   
-  byte transmit = data ^ B10101010;
+  byte transmit = data ^ B10101010; // Use a bitwise XOR to apply a basic modulation to the data
   
-  Serial.write(transmit);
+  Serial.write(transmit); // Send the data
 }
