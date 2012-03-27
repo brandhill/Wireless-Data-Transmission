a205085dc2454138e0aaa8fe1c85bd711789246e
Zachary Liu <liu.zachary@gmail.com>
2012-03-27 17:26:47 -0400

    Wrote code implementing wireless data transmission with data modulation.

--- reciever/reciever.ino
+++ reciever/reciever.ino
@@ -1,1 +1,17 @@
+void setup() {
+  Serial.begin(2400);
+  pinMode(13, OUTPUT);
+}
 
+void loop() {
+  if (Serial.available()) {
+    byte recieved = Serial.read();
+    byte data = recieved ^ B10101010;
+    
+    if (data == B00000001) {
+      digitalWrite(13, HIGH);
+    } else if (data == B00000000) {
+      digitalWrite(13, LOW);
+    }
+  }
+}
--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,1 +1,18 @@
+void setup() {
+  Serial.begin(2400);
+  pinMode(2, INPUT);
+}
 
+void loop() {
+  byte data;
+  
+  if (digitalRead(2) == HIGH) {
+    data = B00000001;
+  } else {
+    data = B00000000;
+  }
+  
+  byte transmit = data ^ B10101010;
+  
+  Serial.write(transmit);
+}
